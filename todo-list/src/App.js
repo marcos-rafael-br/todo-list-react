f69b7e60f06f1f6619cab7a1d02ad6136891e5c4
marcos-rafael-br <mrpf10@hotmail.com>
2022-05-02 15:27:19 -0300

    logica para adicionar tasks

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,6 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [todo, setTodo] = useState([]);
+
+  const handleChange = ({ target }) => {
+    setValue(target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodo([...todo, value]);
+    setValue("");
+  };
+
   return (
     <div class="container">
       <div class="date-container">
@@ -8,7 +22,13 @@ function App() {
         <h5 class="month">March</h5>
         <h1 class="number-of-tasks">0 Tasks</h1>
       </div>
-      <div class="task-list-container"></div>
+      <div class="task-list-container">
+        <ul>
+        {todo.map((item) => {
+          return <li> {item} </li>;
+        })}
+        </ul>
+      </div>
       <div class="tasks-menu-container">
         <span id="all" class="active">
           All
@@ -18,8 +38,16 @@ function App() {
         <button class="clear-all-btn">Clear All</button>
       </div>
       <div class="input-div">
-        <input type="text" class="task-input" placeholder="Enter Your Task" />
-        <button class="add-task-btn">+</button>
+        <input
+          type="text"
+          class="task-input"
+          placeholder="Enter Your Task"
+          onChange={handleChange}
+          value={value}
+        />
+        <button class="add-task-btn" onClick={handleSubmit}>
+          +
+        </button>
       </div>
     </div>
   );
